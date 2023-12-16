a73c7de0ea467a16b72e4d33f2336fb2773cb107
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2023-12-16 22:30:36 +0000

    Update delta_c_find.py

--- delta_c_find.py
+++ delta_c_find.py
@@ -13,19 +13,30 @@ def H_f(a):
         return H0*np.sqrt(1-Omegam0-Omegar0+Omegam0*a**(-3)+Omegar0*a**(-4))
     elif model_H == 'wCDM':
         return H0*np.sqrt((1-Omegam0-Omegar0)*a**(-3*(1+wL))+Omegam0*a**(-3)+Omegar0*a**(-4))
+    elif model_H == 'nDGP':
+        Omegarc = 1/(4*H0**2*rc**2)
+        OmegaLambda0 = 1 - Omegam0 - Omegar0
+        return H0*np.sqrt(Omegam0*a**(-3)+Omegar0*a**(-4)+Omegarc+OmegaLambda0)-H0*np.sqrt(Omegarc)
 
 def dH_f(a):
     if model_H == 'LCDM':
         return -0.5*(H0*(3*a*Omegam0 + 4*Omegar0))/(a**5*np.sqrt((a*Omegam0 + Omegar0 - a**4*(-1 + Omegam0 + Omegar0))/a**4))
     elif model_H == 'wCDM':
         return (a**(-5 - 3*wL)*(3*a*H0*(1 + wL)*(-1 + Omegam0 + Omegar0) - a**(3*wL)*H0*(3*a*Omegam0 + 4*Omegar0)))/(2*np.sqrt((Omegar0 + a*(Omegam0 - (-1 + Omegam0 + Omegar0)/a**(3*wL)))/a**4))
-
+    elif model_H == 'nDGP':
+        Omegarc = 1/(4*H0**2*rc**2)
+        OmegaLambda0 = 1 - Omegam0 - Omegar0
+        return -0.5*(H0*(3*a*Omegam0 + 4*Omegar0))/(a**5*np.sqrt((a*Omegam0 + Omegar0)/a**4 + Omegarc + OmegaLambda0))
+        
 def mu(par1, par2, a):
     H = H_f(a)
+    dHda = dH_f(a)
+    dHdt = a*H*dHda
     rhom = 3*H0**2*Omegam0*a**(-3)
     rhor = 3*H0**2*Omegar0*a**(-4)
     rhoL = 3*H**2-rhom-rhor
     OmegaL = rhoL/(3*H**2)
+    OmegaM = rhom/(3*H**2)
     if model == 'E11':
         f1 = par1*OmegaL
         return 1 + f1
@@ -33,8 +44,14 @@ def mu(par1, par2, a):
         return 1+ par1*(1-a)**1 - par1*(1-a)**2
     elif model == 'DES':
         return 1 + par1*OmegaL + par2*OmegaL**2
-
-
+    elif model == 'wCDM':
+        return 2/3*OmegaM**(gamma-1)*(OmegaM**gamma+2-3*gamma+3*(gamma-1/2)*(OmegaM+(1+wL)*OmegaL))
+    elif model == 'nDGP':
+        OmegaLambda0 = 1 - Omegam0 - Omegar0
+        Omegarc = 1/(4*H0**2*par1**2)
+        beta = 1 + (Omegam0*a**(-3)+Omegar0*a**(-4)+2*OmegaLambda0)/(2*np.sqrt(Omegarc*(Omegam0*a**(-3)+Omegar0*a**(-4)+OmegaLambda0)))
+        return 1 + 1/(3*beta)
+        
 def delta_nl_ODE(a, y, par1, par2):
     delta,ddeltada = y
     H = H_f(a)
