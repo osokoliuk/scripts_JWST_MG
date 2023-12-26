0f91a92399975e53adb43f76b820bfaaae57be66
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2023-12-26 14:51:36 +0000

    Update delta_c_find.py

--- delta_c_find.py
+++ delta_c_find.py
@@ -10,12 +10,59 @@ from classy import Class
 from scipy.optimize import fsolve
 import math
 import scipy
+from tqdm import tqdm 
+
+common_settings = {'A_s':2.101e-9,
+          'n_s':0.9665,
+          'tau_reio':0.0561,
+          'omega_b':0.02242,
+          'omega_cdm':0.11933,
+          'h':0.6766,
+          'YHe':0.2425,
+          'T_cmb':2.7255,
+          'gauge':'newtonian', #FOR MGCLASS TO WORK, GAUGE NEEDS TO BE NEWTONIAN
+          'k_pivot': 0.05,
+          'mg_z_init': 111111.000,
+          'l_logstep': 1.025,
+          'l_linstep':15,
+          'P_k_max_1/Mpc':3.0,
+          'l_switch_limber':9,
+          'perturb_sampling_stepsize': 0.05,
+          'output':'tCl,pCl,lCl,mPk',
+          'l_max_scalars': 3000,
+          'lensing': 'yes',
+          'mg_ansatz':'kmoufl'}
+
 
 H0 = 67.66
 Omegam0 = (0.02242/(H0/100)**2+0.11933/(H0/100)**2)
 Omegar0 = 8.493e-5
 c = 299792.45800000057
 
+
+K0 = [0, 0.5, 1]
+beta = np.linspace(0,0.5,10)
+H_arr_kmoufl = [[],[],[]]
+dH_arr_kmoufl = [[],[],[]]
+H_int_kmoufl = [[],[],[]]
+dH_int_kmoufl = [[],[],[]]
+
+M = {}
+for i in range(len(K0)):
+    for j in tqdm(range(len(beta))):
+        common_settings['beta_kmfl'] = beta[j]
+        common_settings['k0_kmfl'] = K0[i]
+
+        M[j] = Class()
+        M[j].set(common_settings)
+        
+        M[j].compute()
+        a = np.logspace(-6,0,10000)
+        H_arr_kmoufl[i].append([M[j].Hubble(1/ai-1)*c for ai in a])
+        dH_arr_kmoufl[i].append(np.gradient(H_arr_kmoufl[i][j])/np.gradient(a))
+        H_int_kmoufl[i].append(scipy.interpolate.interp1d(a, H_arr_kmoufl[i][j], fill_value = 'extrapolate'))
+        dH_int_kmoufl[i].append(scipy.interpolate.interp1d(a, dH_arr_kmoufl[i][j], fill_value = 'extrapolate'))
+        
 def H_f(a):
     if model_H == 'LCDM':
         return H0*np.sqrt(1-Omegam0-Omegar0+Omegam0*a**(-3)+Omegar0*a**(-4))
@@ -25,14 +72,8 @@ def H_f(a):
         Omegarc = 1/(4*H0**2*rc**2)
         OmegaLambda0 = 1 - Omegam0 - Omegar0 + 2*np.sqrt(Omegarc)
         return H0*np.sqrt(Omegam0*a**(-3)+Omegar0*a**(-4)+Omegarc + OmegaLambda0)-H0*np.sqrt(Omegarc)
-    elif model_H == 'kmouflage':
-        Omegaphi0 = 1 - Omegam0 - Omegar0
-        e2 = a*beta/(1+a*beta)
-        A = 1 + beta*phi
-        K = -1 + chi + K0*chi0**2
-        rhophi = M**4/A**4*(2*chi*Kprime-K)
-        rhophi0 = M**4*(2*chi0*Kprime-K)
-        return H0*A*(1-e2)*np.sqrt(Omegam0*a**(-3)+Omegar0*a**(-4)+Omegaphi0*rhophi/rhophi0)
+    elif model_H == 'kmoufl':
+        return H_int_kmoufl[i_kmoufl][j_kmoufl](a)
 
 def dH_f(a):
     if model_H == 'LCDM':
@@ -43,6 +84,8 @@ def dH_f(a):
         Omegarc = 1/(4*H0**2*rc**2)
         OmegaLambda0 = 1 - Omegam0 - Omegar0 + 2*np.sqrt(Omegarc)
         return  (H0*((-3*Omegam0)/a**4 - (4*Omegar0)/a**5))/(2.*np.sqrt(OmegaLambda0 + Omegam0/a**3 + Omegar0/a**4 + Omegarc))
+    elif model_H == 'kmoufl':
+        return dH_int_kmoufl[i_kmoufl][j_kmoufl](a)
         
 def mu(par1, par2, a):
     H = H_f(a)
@@ -65,7 +108,14 @@ def mu(par1, par2, a):
     elif model == 'nDGP':
         beta = 1 + 2*H/c*par1*(1+dHdt/(3*H**2))
         return 1 + 1/(3*beta)
-        
+    elif model == 'kmoufl':
+        A_kmfl = 1.0 + par1*a
+        X_kmfl = 0.5 * A_kmfl**2*(H*a)**2/((1-Omegam0-Omegar0)*H0**2)
+        k_prime_mfl = 1.0 + 2.0*par2*X_kmfl
+        epsl1_kmfl = 2.0*par1**2/k_prime_mfl    
+        X_kmfl_dot = 0.5 * A_kmfl**2/((1-Omegam0-Omegar0)*H0**2)*2.0*H*a*(H*H*a+dHdt*a)
+        return 1.+ epsl1_kmfl
+
 def delta_nl_ODE(a, y, par1, par2):
     delta,ddeltada = y
     H = H_f(a)
