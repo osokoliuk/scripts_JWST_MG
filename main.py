bea30ea20b5a2de0b4bb0ff8e1314265c9b383a7
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2024-02-01 16:06:21 +0000

    new

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from JWST_MG.cosmological_functions import cosmological_library
+model = 'nDGP'
+model_H = 'nDGP'
+
+par1 = 15000
+par2 = 1
+
+library = cosmological_library(1, model, model_H, par1, par2)
+z = 2
+print(library.dH_f(1/(1+z), model_H, par1, par2))
 
