11d3c0c99f9686115c3efd0ff93d223ee1369213
Oleksii Sokoliuk <oleksii.sokoliuk@mao.kiev.ua>
2024-06-07 00:44:42 +0100

    updates

--- figures/SMF_nDGP_Puebla.py
+++ figures/SMF_nDGP_Puebla.py
@@ -358,7 +358,7 @@ for z_smf in z_smf_arr:
     par2 = 0
     f0 = 0.21
     n = len(pars1)
-    cmap3 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","#8da0cb"])
+    cmap3 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","#48639e"])
 
     colors = cmap3(np.linspace(0, 1, n))
     
