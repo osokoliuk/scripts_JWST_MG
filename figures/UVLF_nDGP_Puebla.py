e885679c32039e7ccd863f46948ba09e582aeffa
Oleksii Sokoliuk <oleksii.sokoliuk@mao.kiev.ua>
2024-06-23 15:30:58 +0100

    updates

--- figures/UVLF_nDGP_Puebla.py
+++ figures/UVLF_nDGP_Puebla.py
@@ -266,6 +266,122 @@ ax.fill_between([-0.1, 1.1], 1.675-0.025, 1.675 +
 plt.xlim(-0.05, 1.05)
 """
 
+from matplotlib.text import Annotation
+from matplotlib.transforms import Affine2D
+
+
+class LineAnnotation(Annotation):
+    """A sloped annotation to *line* at position *x* with *text*
+    Optionally an arrow pointing from the text to the graph at *x* can be drawn.
+    Usage
+    -----
+    fig, ax = subplots()
+    x = linspace(0, 2*pi)
+    line, = ax.plot(x, sin(x))
+    ax.add_artist(LineAnnotation("text", line, 1.5))
+    """
+
+    def __init__(
+        self, text, line, x, xytext=(0, 5), textcoords="offset points", **kwargs
+    ):
+        """Annotate the point at *x* of the graph *line* with text *text*.
+
+        By default, the text is displayed with the same rotation as the slope of the
+        graph at a relative position *xytext* above it (perpendicularly above).
+
+        An arrow pointing from the text to the annotated point *xy* can
+        be added by defining *arrowprops*.
+
+        Parameters
+        ----------
+        text : str
+            The text of the annotation.
+        line : Line2D
+            Matplotlib line object to annotate
+        x : float
+            The point *x* to annotate. y is calculated from the points on the line.
+        xytext : (float, float), default: (0, 5)
+            The position *(x, y)* relative to the point *x* on the *line* to place the
+            text at. The coordinate system is determined by *textcoords*.
+        **kwargs
+            Additional keyword arguments are passed on to `Annotation`.
+
+        See also
+        --------
+        `Annotation`
+        `line_annotate`
+        """
+        assert textcoords.startswith(
+            "offset "
+        ), "*textcoords* must be 'offset points' or 'offset pixels'"
+
+        self.line = line
+        self.xytext = xytext
+
+        # Determine points of line immediately to the left and right of x
+        xs, ys = line.get_data()
+
+        def neighbours(x, xs, ys, try_invert=True):
+            inds, = np.where((xs <= x)[:-1] & (xs > x)[1:])
+            if len(inds) == 0:
+                assert try_invert, "line must cross x"
+                return neighbours(x, xs[::-1], ys[::-1], try_invert=False)
+
+            i = inds[0]
+            return np.asarray([(xs[i], ys[i]), (xs[i+1], ys[i+1])])
+        
+        self.neighbours = n1, n2 = neighbours(x, xs, ys)
+        
+        # Calculate y by interpolating neighbouring points
+        y = n1[1] + ((x - n1[0]) * (n2[1] - n1[1]) / (n2[0] - n1[0]))
+
+        kwargs = {
+            "horizontalalignment": "center",
+            "rotation_mode": "anchor",
+            **kwargs,
+        }
+        super().__init__(text, (x, y), xytext=xytext, textcoords=textcoords, **kwargs)
+
+    def get_rotation(self):
+        """Determines angle of the slope of the neighbours in display coordinate system
+        """
+        transData = self.line.get_transform()
+        dx, dy = np.diff(transData.transform(self.neighbours), axis=0).squeeze()
+        return np.rad2deg(np.arctan2(dy, dx))
+
+    def update_positions(self, renderer):
+        """Updates relative position of annotation text
+        Note
+        ----
+        Called during annotation `draw` call
+        """
+        xytext = Affine2D().rotate_deg(self.get_rotation()).transform(self.xytext)
+        self.set_position(xytext)
+        super().update_positions(renderer)
+
+
+def line_annotate(text, line, x, *args, **kwargs):
+    """Add a sloped annotation to *line* at position *x* with *text*
+
+    Optionally an arrow pointing from the text to the graph at *x* can be drawn.
+
+    Usage
+    -----
+    x = linspace(0, 2*pi)
+    line, = ax.plot(x, sin(x))
+    line_annotate("sin(x)", line, 1.5)
+
+    See also
+    --------
+    `LineAnnotation`
+    `plt.annotate`
+    """
+    ax = line.axes
+    a = LineAnnotation(text, line, x, *args, **kwargs)
+    if "clip_on" in kwargs:
+        a.set_clip_path(ax.patch)
+    ax.add_artist(a)
+    return a
 
 
 #plt.errorbar(x, y, yerr=yerr, xerr=xerr, fmt='.')
@@ -517,7 +633,7 @@ fig = plt.figure(figsize=(4.25*2*.95, 2*4*1.05))
 
 
 nn = 1
-z_smf_arr = [4]
+z_smf_arr = [9]
 
 for z_smf in z_smf_arr:
     ax_Pk = plt.subplot(4,2,1)
@@ -628,17 +744,8 @@ for z_smf in z_smf_arr:
     iterable = [(1/(1+z_smf), rhom, model, model_H, model_SFR, par1, par2, Masses, k, Pk_arr[0], f0, sigma_uv) for sigma_uv in sigmas]
     MUV, UVLF_obs = zip(*pool_cpu.starmap(UVLF_library.compute_uv_luminosity_function,tqdm(iterable, total=len(sigmas))))
     for i in range(len(UVLF_obs)):
-        ax_Pk.plot(MUV[i], UVLF_obs[i], c = 'k', lw=4, alpha=0.2)
-
-        if sigmas[i] == 2:
-            id = np.arange(len(MUV[i]))[np.log10(UVLF_obs[i]) > -4.75][-1]
-            if MUV[i][id] + 0.1 < -22.5:
-                plt.text( MUV[i][id] + 0.1, UVLF_obs[i][id]*0.4, r"$\sigma_{\rm UV} = "+str(sigmas[i]) + "$", fontsize=10, color='tab:gray', rotation=22, zorder=-1)
-        else:
-            id = np.arange(len(MUV[i]))[np.log10(UVLF_obs[i]) > -3.5][-1]
-            if MUV[i][id] + 0.1 < -20:
-                plt.text( MUV[i][id] + 0.1, UVLF_obs[i][id]*2.5, r"$\sigma_{\rm UV} = "+str(sigmas[i]) + "$", fontsize=10, color='tab:gray', rotation=15.7, zorder=-1)
-
+        line, = ax_Pk.plot(MUV[i], UVLF_obs[i], c = 'k', lw=4, alpha=0.2)
+        line_annotate(r'$\sigma_{\rm UV}=' + str(sigmas[i]) + '$',line,-23, c = 'tab:gray', fontsize = 9)
 
 
 
@@ -770,16 +877,7 @@ for z_smf in z_smf_arr:
     MUV, UVLF_obs = zip(*pool_cpu.starmap(UVLF_library.compute_uv_luminosity_function,tqdm(iterable, total=len(sigmas))))
     for i in range(len(UVLF_obs)):
         ax_Pk.plot(MUV[i], UVLF_obs[i], c = 'k', lw=4, alpha=0.2)
-
-        if sigmas[i] == 2:
-            id = np.arange(len(MUV[i]))[np.log10(UVLF_obs[i]) > -4.75][-1]
-            if MUV[i][id] + 0.1 < -22.5:
-                plt.text( MUV[i][id] + 0.1, UVLF_obs[i][id]*0.4, r"$\sigma_{\rm UV} = "+str(sigmas[i]) + "$", fontsize=10, color='tab:gray', rotation=22, zorder=-1)
-        else:
-            id = np.arange(len(MUV[i]))[np.log10(UVLF_obs[i]) > -3.5][-1]
-            if MUV[i][id] + 0.1 < -20:
-                plt.text( MUV[i][id] + 0.1, UVLF_obs[i][id]*2.5, r"$\sigma_{\rm UV} = "+str(sigmas[i]) + "$", fontsize=10, color='tab:gray', rotation=15.7, zorder=-1)
-
+        line_annotate(r'$\sigma_{\rm UV}=' + str(sigmas[i]) + '$',line,-23, c = 'tab:gray', fontsize = 9)
 
 
     #plt.errorbar(x.get('Duncan'),y.get('Duncan'),yerr=[yerr_down.get('Duncan'),yerr_up.get('Duncan')], c = 'tab:orange', capsize = 2, ls = 'None', marker = '.', label = r'$\rm Duncan+14$')
@@ -813,6 +911,7 @@ for z_smf in z_smf_arr:
     ax_Pk.add_artist(legend1)
     
     nn += 1
+
 
 plt.tight_layout()
 
